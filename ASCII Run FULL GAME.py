6c070b6e297f57cae29e741706e0ae77928af262
Marshall Nielsen <marshallnielsen7@gmail.com>
2025-01-02 14:59:47 -0500

    Initial Commit

---
+++ ASCII Run FULL GAME.py
@@ -0,0 +1,321 @@
+import time
+import glob
+import keyboard
+import random
+#from intertools import groupby
+
+animation = []
+projectile_index = []
+bullet_index = []
+character_frame = 0
+distance = 0
+current_jump_height = 0
+distance_from_top = 0
+lives = 5
+
+character = "Character Files\\Guy.txt"
+
+jump_counter = 0
+
+def intro():
+    f = open("Intro_Sequence.txt")
+    f_contents = f.read()
+    f.close()
+    sequence = f_contents.splitlines()
+    print("\n\n\n\n")
+    for line in sequence:
+        print(line)
+        print("\n")
+        time.sleep(3)
+
+def select_character():
+    global character
+    print('\nChoose your warrior...\n')
+    options = glob.glob('Character Files/*.txt')
+    for option in options:
+        option = option.replace('.txt', '')
+        option = option.replace('Character Files\\', '')
+        print(option)
+    character = 'Character Files\\' + input('\nEnter character name: ') + '.txt'
+    
+
+def fetch_animation_pane():
+    f = open("Mountains.txt")
+    f_contents = f.read()
+    f.close()
+    return f_contents.splitlines()
+
+def fetch_character_animation():
+    f = open(character)               #fetch the file with the character animation
+    f_contents = f.read()
+    f.close()
+    by_frames = f_contents.split('+')    #turn it into a list of strings, each string being a frame
+    guy_animation = []
+    for element in by_frames:             #turn each frame string into a list, each list being a list of the rows in the frame
+        if element.startswith("\n"):
+            element = element[1:]
+        guy_animation.append(element.splitlines())
+    return guy_animation
+    
+def fetch_dragon():
+    f = open("Dragon.txt")               #fetch the file with the character animation
+    f_contents = f.read()
+    f.close()
+    by_frames = f_contents.split('+')    #turn it into a list of strings, each string being a frame
+    dragon_animation = []
+    for element in by_frames:             #turn each frame string into a list, each list being a list of the rows in the frame
+        if element.startswith("\n"):
+            element = element[1:]
+        dragon_animation.append(element.splitlines())
+    return dragon_animation
+
+def print_animation():
+    for line in animation:
+        print(line)
+    print("")
+
+def add_character():
+    global animation
+    global character_frame
+    global distance_from_top
+    
+    guy_animation = list(character_animation)
+    
+    if character_frame < (len(guy_animation) - 1):
+        character_frame = character_frame + 1
+    else:
+        character_frame = 0
+        
+    frame_lists = guy_animation[character_frame]      #extract the list of rows in the current frame
+    
+    character_height = len(frame_lists)
+    pane_height = len(animation) - 1
+    airspace = pane_height - character_height
+    
+    distance_from_top = airspace - current_jump_height #modifies global variable for use in projectile sensing
+    
+    add_image(frame_lists, 0, distance_from_top)
+
+def update_character_height():  
+    global jump_counter
+    global current_jump_height
+    
+    if jump_counter > 0:     #ascending
+        if distance_from_top > 0:
+            current_jump_height = current_jump_height + 1
+        jump_counter = jump_counter - 1  
+        
+    elif jump_counter == 0 and current_jump_height == 0:   #on the ground
+        current_jump_height = current_jump_height
+    elif jump_counter == 0 and current_jump_height > 0:   #descending
+        current_jump_height = current_jump_height - 1
+
+
+def print_heading():
+    print("Distance: " + str(distance) + "       " + "Lives: " + str(lives) + "       " + "Dragon: " + str(dragon_states[0]))
+
+
+def jump():
+    global jump_counter
+    jump_counter = 5
+    
+def add_projectiles():
+    global projectile_index
+    global animation
+    global lives
+    global dragonstates
+    seed = random.randint(1,6)
+    if seed == 5:                          #creates a new arrow if the seed is 5
+        projectile_index.append([random.randint(0,(len(animation)-4))   ,   len(animation[1])   ,  '<'])
+        
+    if dragon_states[3] == 'blowing':
+        for x in range(41):
+            projectile_index.append([dragon_states[0]+7, x, 'C'])
+    else:
+        for projectile in projectile_index:
+            if projectile[2] == 'C':
+                projectile_index.remove(projectile)
+        
+    temp = []                                 #removes duplicates
+    for projectile in projectile_index:
+        if projectile not in temp:
+            temp.append(projectile)
+    projectile_index = temp
+    
+    bullet_index = []                        #creates new index for where the bullets are
+    for projectile in projectile_index:
+        if projectile[2] == '=':
+            new_entry = [projectile[0], projectile[1]]
+            bullet_index.append(new_entry)
+    
+    for projectile in projectile_index:
+        row = projectile[0]
+        column = projectile[1]
+        if projectile[2] == '<':           #for arrows   
+            if column <= 2:                 #deletes them at the end of the screen
+                projectile_index.remove(projectile)
+            if column <= len(character_animation[0][0]):  #checks the arrow for contact
+                if (row >= distance_from_top) and (row < (distance_from_top + len(character_animation[0]))):
+                    lives = lives - 1
+                    try:
+                        projectile_index.remove(projectile)
+                    except:
+                        print("that thing happened")
+            for bullet in bullet_index:   #checks the arrow for a collision
+                if row == bullet[0] and column <= bullet[1] + 1:
+                    try:
+                        projectile_index.remove(projectile)
+                    except:
+                        print("that thing happened")
+                    for possible_same_line_bullet in projectile_index:
+                        if possible_same_line_bullet[0] == row and possible_same_line_bullet[2] == "=":
+                            projectile_index.remove(possible_same_line_bullet)
+                            break
+            projectile[1] = projectile[1] - 1    #advances the arrow one space
+        
+        if projectile[2] == '=':   #for bullets
+            projectile[1] = projectile[1] + 1
+            if column >= len(animation[0]):                 #deletes them at the end of the screen
+                projectile_index.remove(projectile)
+        
+        if projectile[2] == 'C':   #for fire
+            if column <= len(character_animation[0][0]):  #checks fire for contact
+                if (row >= distance_from_top) and (row < (distance_from_top + len(character_animation[0]))):
+                    lives = lives - 1  
+              
+        #generic printing of each projetile
+        animation[row] = animation[row][0:(column-1)] + projectile[2] + animation[row][column:len(animation[1])]
+
+                
+def shoot():
+    global projectile_index
+    possible_bullet = [distance_from_top, 5, '=']
+    projectile_index.append(possible_bullet)
+    
+    
+
+def starting_art():
+    f = open("Starting art.txt")               #fetch the file with the character animation
+    f_contents = f.read()
+    f.close()
+    print(f_contents)
+
+def end_game():
+    input("\n Press enter to play again ")
+    reset_game()
+
+
+def reset_game():
+    global distance
+    global lives
+    global current_jump_height
+    global projectile_index
+    global dragon_states
+    distance = 0
+    lives = 5
+    current_jump_height = 0
+    projectile_index.clear()
+    dragon_states = [-15, 'falling', 5, 'flying', 10]
+
+def create_frame():
+    global distance
+    scene = list(background)
+    frame = []
+    start = distance / 10
+    end = start + 80
+    for line in scene:
+        needed_portion = line[int(start):int(end)]
+        frame.append(needed_portion)
+    return frame
+
+def add_dragon():
+    global animation
+    global dragon_animation
+    ycoord = update_dragon_height()
+    dragon_frames = list(dragon_animation)
+    if dragon_states[3] == 'flying':
+        frame = (distance % 4)
+        add_image(dragon_frames[frame], 30, ycoord)
+    elif dragon_states[3] == 'blowing':
+        add_image(dragon_frames[4], 30, ycoord)
+    
+def update_dragon_height():
+    global dragon_height
+    global distance
+    global dragon_states
+    
+    if distance < 10:
+        return 0
+    elif(distance < 25):
+        return -(distance - 10)
+    elif(dragon_states[3] == 'flying'):
+        if(dragon_states[1] == 'falling'):
+            dragon_states[0] += 1
+            if dragon_states[0] >= dragon_states[2]:
+                dragon_states[1] = 'rising'
+                dragon_states[3] = 'blowing'
+        if(dragon_states[1] == 'rising'):
+            dragon_states[0] += -1
+            if dragon_states[0] == -len(dragon_animation[1]):
+                dragon_states[1] = 'falling'
+                dragon_states[2] = random.randint(0, int(len(dragon_animation[1]) / 2))
+        return dragon_states[0]
+    elif(dragon_states[3] == 'blowing'):
+        dragon_states[4] -= 1
+        if dragon_states[4] == 0:
+            dragon_states[4] = 10
+            dragon_states[3] = 'flying'
+        return dragon_states[0]
+        
+    
+
+def add_image(image, xcoord, ycoord):
+    global animation
+    for line in image:
+        if ((ycoord + image.index(line)) >= 0) and ((ycoord + image.index(line)) <= (len(animation) - 1)):
+            animation_line = animation[ycoord + image.index(line)]
+            line_length = len(line)
+            leading_space = len(line) - len(line.lstrip())
+            trailing_space = len(line) - len(line.rstrip())
+            addition_length = len(line.strip())
+            addition = line.strip()
+            addition_start_point = xcoord + leading_space
+            addition_end_point = xcoord + leading_space + addition_length
+            animation[ycoord + image.index(line)] = animation_line[0:addition_start_point] + addition + animation_line[addition_end_point:]
+
+    
+        
+        
+dragon_states = [-15, 'falling', 5, 'flying', 10] 
+#dragon's y coordinate, whether rising or falling, target y coordinate, flying or blowing, stagnation counter
+
+intro()
+starting_art()
+
+select_character()
+
+background = tuple(fetch_animation_pane())
+character_animation = tuple(fetch_character_animation())
+dragon_animation = tuple(fetch_dragon())
+
+keyboard.add_hotkey('space', jump)   #set up the spacebar as a jumping hotkey
+keyboard.add_hotkey('ctrl', shoot)
+
+while True:
+    animation = create_frame()
+    update_character_height()
+    
+    add_character()
+    add_projectiles()
+    add_dragon()
+    
+    print_heading()
+    print_animation()
+    
+    if lives <= 0:
+        end_game()
+    
+    time.sleep(0.25)
+    distance = distance + 1
+
+
---
+++ Tic Tac Toe.py
@@ -0,0 +1,221 @@
+#the current state of played xes and os in the game
+import sys
+import random
+import pandas as pd
+from datetime import datetime
+
+
+current_board = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
+
+pick_coordinate = 0
+
+game_data = {} #keys are the computer's turn number, followed by the gameboard, 
+                #the decision, whether playing x or o, and the timestamp
+turn_number = 0
+winner = ""
+
+winning_combos = [[0,1,2],
+                  [3,4,5],
+                  [6,7,8],
+                  [0,3,6],
+                  [1,4,7],
+                  [2,5,8],
+                  [0,4,8],
+                  [2,4,6]]
+
+def display():
+    print(current_board[0], current_board[1], current_board[2])
+    print(current_board[3], current_board[4], current_board[5])
+    print(current_board[6], current_board[7], current_board[8])
+    
+def human_turn(x_or_o):
+    pick = input('Pick a spot to play (ex. "top right") ')
+    pick.lower()
+    pick_coordinate = (0 if pick.startswith("top") else 3 if 
+        pick.startswith("middle" or "center") else 6 if 
+        pick.startswith("bottom") else 0)
+    pick_coordinate = pick_coordinate + (0 if pick.endswith("left") else 1 if 
+        pick.endswith("middle" or "center") else 2 if 
+        pick.endswith("right") else 0)
+    
+    if pick.startswith("end"):
+        sys.exit()
+    
+    if current_board[pick_coordinate] == "-":
+        current_board[pick_coordinate] = x_or_o
+    
+def my_turn_random(x_or_o):
+    global turn_number
+    while True:
+        my_pick = random.randint(0, 8)
+        if current_board[my_pick] == "-":
+            break #once the computer has picked a valid spot
+    timestamp = datetime.now()
+    stamp = str(current_board)
+    entry = {"gameboard_stamp": stamp, "pick": my_pick, "x_or_o": x_or_o, "victory": "in progress", "timestamp": timestamp}
+    game_data[turn_number] = entry
+    turn_number = turn_number + 1
+    current_board[my_pick] = x_or_o;
+    
+def my_turn_smart(x_or_o):
+    global turn_number
+    global current_board
+    df = pd.read_csv("tic tac toe insights.csv", index_col=0)
+    
+    data_dictionary = df.to_dict(orient="index")
+    #produces a dictionary with the insights gameboards as keys with an entry that is a
+    #dictionary that uses the different choices as keys
+    
+    generic_current_board = current_board
+    for i in range(len(generic_current_board)): #convert to a generic form
+        if generic_current_board[i] == x_or_o:
+            generic_current_board[i] = "M"
+        elif generic_current_board[i] == ("X" if x_or_o=="O" else "O" if x_or_o=="X" else "-"):
+            generic_current_board[i] = "Y"
+    
+    if str(generic_current_board) in data_dictionary:
+        situation_study = data_dictionary[str(generic_current_board)]
+        print(situation_study)
+        my_pick = insightful_decision(situation_study)
+    else: #new situation, guess randomly
+        while True:
+            my_pick = random.randint(0, 8)
+            if current_board[my_pick] == "-":
+                break #once the computer has picked a valid spot
+    
+    for i in range(9): #convert back to specific form, not sure why I need to
+        if current_board[i] == "M":
+            current_board[i] = x_or_o
+        elif current_board[i] == "Y":
+            current_board[i] = ("X" if x_or_o=="O" else "O" if x_or_o=="X" else "-")
+    
+    timestamp = datetime.now()
+    entry = current_board + [my_pick] + [x_or_o] + ["in progress"] + [timestamp]
+    game_data[turn_number] = entry
+    turn_number = turn_number + 1
+    current_board[my_pick] = x_or_o;
+    
+    input("Hit Enter ")
+   
+def insightful_decision(situation_study):
+    choice_list = []
+    choice_weights = []
+    for choice in situation_study:
+        if situation_study[choice] != None:
+            choice_list = choice_list + [choice]
+            print(situation_study[choice])
+            weight = situation_study[choice][1] #still don't know what's goint on here
+            print(weight)
+            #choice_weights = choice_weights + [weight]
+            
+    print(choice_list, choice_weights)
+    input("Hit Enter ")
+        
+    
+            
+def check_for_win(): #also checks for a tie
+    global winner
+    for combo in winning_combos:
+        if (current_board[combo[0]] == current_board[combo[1]] == current_board[combo[2]] != "-"):
+            print("GAME OVER")
+            winner = current_board[combo[0]] #returns the character of who won
+            print("Winner: ", winner)
+            print("Winning combo: ", combo)
+            return True
+    if "-" not in current_board:
+        winner = "tie"
+        print("Winner: ", winner)
+        return True
+    return False
+
+def log_game():
+    game_df = pd.DataFrame(data=game_data).T #the log from this game
+    old_df = pd.DataFrame(game_data).T  #the log from all of the previous games
+    df = pd.concat([game_df, old_df])
+    df.to_csv("tic tac toe experience.csv", mode="a", header=False, index=True) #new log with both together
+            
+
+def generate_insights():
+    
+    insights = {}
+
+    df = pd.read_csv("tic tac toe experience.csv")
+
+    data_dictionary = df.to_dict(orient="split")
+    data = data_dictionary["data"] #ends up with a list of lists of the actual data
+
+    for turn in data:
+        gameboard = str(turn[0:9]) #string of the gameboard
+        gameboard_list = turn[0:9]
+        decision = turn[9] #the number of the spot that was chosen
+        won = "won" in turn #becomes true if it was a "won" log entry
+        if gameboard in insights:
+            #this gameboard has been seen before
+            previous_stats = insights[gameboard][decision] #the previous stats for that decision
+            new_number_of_instances = previous_stats[0] + 1
+            new_percent_victory = ((((0 if previous_stats[1] == None else previous_stats[1]) * previous_stats[0]) + 
+                                    (1 if won else 0)) / new_number_of_instances)
+            insights[gameboard][decision] = [new_number_of_instances, new_percent_victory]
+        else:
+            #conrgrats! this board setup has never been seen before
+            #let's study it
+            new_insight = {0:[0, None],1:[0, None],2:[0, None],3:[0, None],
+                           4:[0, None],5:[0, None],6:[0, None],7:[0, None],
+                           8:[0, None]}
+            
+            for position in range(9): #deletes data entry spaces for choices it can't make
+                if gameboard_list[position] != "-":
+                    del(new_insight[position])
+                    
+            new_insight[decision] = [1, 1 if won else 0] #adds that data point
+    
+            insights[gameboard] = new_insight #publishes the new insight to the insights list
+
+    print("insights generated")
+    insights_dataframe = pd.DataFrame(data=insights).T
+    insights_dataframe.to_csv("tic tac toe insights.csv")
+    
+def reset_game():
+    global current_board
+    global pick_coordinate
+    global game_data
+    global turn_number
+    global winner
+    
+    current_board = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
+    pick_coordinate = 0
+    game_data = {} #keys are the computer's turn number, followed by the gameboard and each decision
+    turn_number = 0
+    winner = ""
+    
+
+print("WELCOME TO TIC TAC TOE")
+
+input("Ready to start? Hit Enter to begin... ")
+
+display()
+
+for i in range(10):
+    while True:
+        my_turn_random("X")
+        display()
+        if check_for_win():
+            log_game()
+            #generate_insights()
+            reset_game()
+            break
+            
+        my_turn_random("O")
+        display()
+        if check_for_win():
+            log_game()
+            #generate_insights()
+            reset_game()
+            break
+
+sys.exit()
+
+# TODO make sure that the dataframes combine indexes, finish mapping the log and converting the program
+    
+            
+             
